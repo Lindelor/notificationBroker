709d3a9d228478d748b5592c2b857b32aca2a3bf
Lindelor <leontev150@mail.ru>
2024-05-12 19:49:34 +0400

    Gateway added

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 rootProject.name = "notification-broker"
 
 includeBuild("eureka-server")
+includeBuild("api-gateway")
 includeBuild("sms-notifier")
 includeBuild("email-notifier")
 includeBuild("push-notifier")
