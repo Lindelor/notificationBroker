31d2896ee5eae0cb1f4773bce223723579d4a3d6
Lindelor <leontev150@mail.ru>
2024-05-12 18:11:30 +0400

    Add eureka

---
+++ eureka-server/src/test/java/io/lind/notification/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lind.notification.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
