10aed7597ec3cf6e801ff8ccbcc0dfeb5b8aea83
Lindelor <leontev150@mail.ru>
2024-05-13 20:46:08 +0400

    Remove webflux

--- api-gateway/src/main/java/io/lind/notification/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/io/lind/notification/apigateway/ApiGatewayApplication.java
@@ -2,13 +2,12 @@ package io.lind.notification.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
 
 }
